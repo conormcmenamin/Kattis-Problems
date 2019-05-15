ff719d280afd628c4668773e42b2c714412be48c
Conor McMenamin <conorm941@gmail.com>
2019-05-15 14:08:59 +0100

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+package Solutions;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt(), y = scan.nextInt(), n = scan.nextInt();
+		
+		for(int i = 1; i<=n;i++) {
+			if((i%x == 0)&&(i%y==0)) {
+				System.out.println("FizzBuzz");
+			}else if(i%x ==0) {
+				System.out.println("Fizz");
+			}else if(i%y == 0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
