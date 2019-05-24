b53363d4119caf670db7740c03fa8ff7d6adbeda
Conor McMenamin <conorm941@gmail.com>
2019-05-24 21:13:37 +0100

    Add files via upload

---
+++ Stuck_In_A_Time_Loop.java
@@ -0,0 +1,18 @@
+package Solutions;
+
+import java.util.Scanner;
+
+public class Stuck_In_A_Time_Loop {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for(int i =1; i<=n; i++)
+		{
+			System.out.println(i + " Abracadabra");
+		}
+	}
+
+}
